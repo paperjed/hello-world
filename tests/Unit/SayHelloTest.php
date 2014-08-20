351d7907f0f4bcdf8e7e768a4be35888f890507b
paperjed <paperjed@gmail.com>
2014-08-21 02:24:11 +0800

    Wat da static...

--- tests/Unit/SayHelloTest.php
+++ tests/Unit/SayHelloTest.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 class SayHelloTest extends PHPUnit_Framework_TestCase
 {
-	public static function testMustBeEqual()
+	public function testMustBeEqual()
 	{
 		$a = new HelloWorld\SayHello();
 		return $this->assertEquals('Hello World, Composer!',$a->world());
